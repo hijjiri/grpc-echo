8a640c73b5a90b8411d2419aad543a8f484f3132
hijjiri <stash1107@gmail.com>
2025-12-02 20:13:10 +0900

    Update6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,8 @@ import (
 	todosrv "github.com/hijjiri/grpc-echo/internal/todo"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -21,14 +23,25 @@ func main() {
 
 	s := grpc.NewServer()
 
-	// 既存サービスの登録
+	// 各サービスの登録
 	echov1.RegisterEchoServiceServer(s, server.NewEchoServer())
 	todov1.RegisterTodoServiceServer(s, todosrv.NewTodoServer())
 
-	// ★ ここで reflection を有効化
+	// Health サーバー
+	healthServer := health.NewServer()
+	healthpb.RegisterHealthServer(s, healthServer)
+
+	// 全体（サービス名空文字）のステータス
+	healthServer.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+
+	// 各サービス単位のステータス
+	healthServer.SetServingStatus(echov1.EchoService_ServiceDesc.ServiceName, healthpb.HealthCheckResponse_SERVING)
+	healthServer.SetServingStatus(todov1.TodoService_ServiceDesc.ServiceName, healthpb.HealthCheckResponse_SERVING)
+
+	// Reflection
 	reflection.Register(s)
 
-	log.Println("gRPC server (Echo + Todo) listening on :50051")
+	log.Println("gRPC server (Echo + Todo + Health) listening on :50051")
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
